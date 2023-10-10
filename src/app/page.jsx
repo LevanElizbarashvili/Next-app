ed08c6c7ca04a97aad6857956961cdd6485d9673
Levan Elizbarashvili <leveliz90@gmail.com>
2023-10-10 17:30:58 +0400

    added home page content

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,22 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import hero from "public/hero.png";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>
+          Better design for your digital products
+        </h1>
+        <p className={styles.description}>
+          turning ideas into beautiful digital products. We bring your ideas to
+          life
+        </p>
+        <button className={styles.button}>See Our Works</button>
+      </div>
+      <div className={styles.item}></div>
+      <Image src={hero} alt="hero" className={styles.hero} />
+    </div>
+  );
 }
