d5b394e178994947d68c30dabebae3b9b184bb5f
Levan Elizbarashvili <leveliz90@gmail.com>
2024-01-17 15:48:58 +0400

    added post add feature

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -5,15 +5,19 @@ import styles from "./page.module.css";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import useSWR from "swr";
+import Image from "next/image";
 
 const dashboard = () => {
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
-  const { data, error, isLoading } = useSWR("", fetcher);
-
   const session = useSession();
 
   const router = useRouter();
+
+  const { data, error, isLoading } = useSWR(
+    `/api/posts?username=${session?.data?.user.name}`,
+    fetcher
+  );
 
   if (session.status === "loading") {
     return <div className={styles.container}>loading</div>;
@@ -21,8 +25,72 @@ const dashboard = () => {
   if (session.status === "unauthenticated") {
     router.push("dashboard/login");
   }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const title = e.target[0].value;
+    const desc = e.target[1].value;
+    const img = e.target[2].value;
+    const content = e.target[3].value;
+
+    try {
+      await fetch("/api/posts", {
+        method: "POST",
+        body: JSON.stringify({
+          title,
+          desc,
+          Image,
+          content,
+          username: session.data.user.name,
+        }),
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   if (session.status === "authenticated") {
-    return <div className={styles.container}>dashboard</div>;
+    return (
+      <div className={styles.container}>
+        <div className={styles.posts}>
+          {isLoading
+            ? "loading..."
+            : data?.map((post) => {
+                <div className="styles.post" key={post.id}>
+                  <div className={styles.imgContainer}>
+                    <Image src={post.image} alt="" />
+                  </div>
+                  <h2 className={styles.postTitle}>{post.title}</h2>
+                  <span
+                    className="styles.delete"
+                    onClick={() => handleDelete(post.id)}
+                  >
+                    X
+                  </span>
+                </div>;
+              })}
+        </div>
+        <form className={styles.new} onSubmit={handleSubmit}>
+          <h1>Add New Post</h1>
+          <input type="text" placeholder="Title" className={styles.input} />
+          <input type="text" placeholder="Desc" className={styles.input} />
+          <input
+            type="text"
+            placeholder="Image"
+            className={styles.input}
+            width={200}
+            height={100}
+          />
+          <textarea
+            cols={30}
+            rows={10}
+            className={styles.textArea}
+            placeholder="content"
+          ></textarea>
+          <button className={styles.button}>Add</button>
+        </form>
+      </div>
+    );
   }
 };
 
